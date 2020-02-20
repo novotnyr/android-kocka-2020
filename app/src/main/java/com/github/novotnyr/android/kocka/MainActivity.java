f69beb9bb81e94d28768f9a760e810f145ab86d2
Robert Novotny <novotnr0@gmail.com>
2020-02-20 23:32:07 +0100

    Pridane tlacidlo ako instancna premenna

--- app/src/main/java/com/github/novotnyr/android/kocka/MainActivity.java
+++ app/src/main/java/com/github/novotnyr/android/kocka/MainActivity.java
@@ -4,15 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private Button rollButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rollButton = findViewById(R.id.rollButton);
     }
 
     public void onRollButtonClick(View view) {
@@ -22,5 +26,4 @@ public class MainActivity extends AppCompatActivity {
         Random random = new Random();
         return random.nextInt(6) + 1; // (0--5) + 1 = 1--6
     }
-
 }
